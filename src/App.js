242048c0584ddab8b110f6a2afc456a0a94a0f65
coder-chris-git <cdav49719@gmail.com>
2022-07-29 03:17:23 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Users from "./components/Users";
+import Login from "./components/Login";
+
+import { AuthContext, AuthProvider } from "./context/AuthContext";
+import { AxiosProvider } from "./context/AxiosContext";
+import SignUp from "./components/SignUp";
+import { GameContext } from "./context/GameContext";
+import AddPlayers from "./components/AddPlayers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const authContext = useContext(AuthContext);
+  const gameContext = useContext(GameContext);
+  const [status, setStatus] = useState("loading");
+
+  const [userState, setUserState] = useState(false);
+  const loadJWT = useCallback(async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const refreshToken = localStorage.getItem("refreshToken");
+      const authenticated = localStorage.getItem("authenticated");
+      const game = localStorage.getItem("game");
+      authContext.setGameState(JSON.parse(game));
+
+      await authContext.setAuthState({
+        accessToken: accessToken || null,
+        refreshToken: refreshToken || null,
+        authenticated: authenticated || null,
+      });
+
+      setStatus("success");
+      setUserState(true);
+    } catch (error) {
+      setStatus("error");
+    }
+  }, []);
+
+  useEffect(() => {
+    loadJWT();
+  }, [loadJWT]);
+  if (authContext?.authState?.authenticated) {
+    return (
+      <>
+        <AddPlayers players={authContext.gameState} />
+
+        {authContext?.gameState?.players !== "undefined" ? <Users /> : "no"}
+      </>
+    );
+  } else {
+    return <Login />;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthContext, AuthProvider } from "./context/AuthContext";
+import { AxiosProvider } from "./context/AxiosContext";
+import { GameContext, GameProvider } from "./context/GameContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <GameProvider>
+      <AxiosProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </AxiosProvider>
+    </GameProvider>
+  </AuthProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
